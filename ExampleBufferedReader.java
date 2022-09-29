bb749449acd0970da1f862560a6c0e61c0f54576
jsandi99 <75376320+jsandi99@users.noreply.github.com>
2022-09-29 14:58:49 +0200

    Add files via upload

---
+++ ExampleBufferedReader.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExampleBufferedReader extends BufferedReader {
+
+    InputStreamReader inputStreamReader;
+    
+    public ExampleBufferedReader(InputStreamReader inputStreamReader) {
+
+        super(inputStreamReader);
+        this.inputStreamReader = inputStreamReader;
+
+    } 
+     
+    public static void setRaw() { // put terminal in raw mode
+        try {
+            Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", "stty raw -echo </dev/tty" });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void unsetRaw() { // restore terminal to cooked mode
+        try {
+            Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", "stty echo cooked </dev/tty" });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args){
+        
+       BufferedReader bufRead = new ExampleBufferedReader(new InputStreamReader(System.in));
+       
+       try{
+       
+       ExampleBufferedReader.setRaw();
+       int i = 0;
+       int c;
+       while((c = bufRead.read()) != '\r'){
+           System.out.println(c);
+       }
+       
+       System.out.print("Hola");
+       
+       
+       ExampleBufferedReader.unsetRaw();
+       } catch (IOException ioe){
+           ioe.printStackTrace();
+       }
+       
+    }    
+}
