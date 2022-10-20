7b74031d0ef714f7425a82a330a3aedca75456fb
oriolmiranda <oriolmiranda15@gmail.com>
2022-10-20 19:37:30 +0200

    Update Line.java

--- P1_Basic/Line.java
+++ P1_Basic/Line.java
@@ -6,13 +6,13 @@ public class Line {
   ArrayList<Character> line;
   int position;
 
-  public Line (){
+  public Line () {
     line = new ArrayList<>();
     insert = false;
     position = 0;
   }
 
-  public void addCharacter(char character){
+  public void addCharacter(char character) {
     if (!insert || position >= line.size()) {
       line.add(position, character);      
       System.out.print("\033[@");
@@ -24,15 +24,15 @@ public class Line {
     position++;
   }
 
-  public void home(){
-    if(position > 0){
+  public void home() {
+    if(position > 0) {
       System.out.print("\033[" + position + "D");
       position = 0;
     }      
   }
 
-  public void end(){
-    if(position < line.size()){
+  public void end() {
+    if(position < line.size()) {
       System.out.print("\033[" + (line.size() - position) + "C");
       position = line.size();
     }
@@ -45,28 +45,26 @@ public class Line {
     } 
   }
 
-  public void left(){
-    if (position > 0){
+  public void left() {
+    if (position > 0) {
       position--;
       System.out.print("\033[D");
     } 
   }
 
-  public void insert(){    
+  public void insert() {    
     insert = !insert;
   }
 
-  public void delete(){
-    if(position < line.size())
-    {
+  public void delete() {
+    if(position < line.size()) {
       line.remove(position);
       System.out.print("\033[P");
     }
   }
 
-  public void backSpace(){
-    if(position > 0)
-    {
+  public void backSpace() {
+    if(position > 0) {
       position--;      
       line.remove(position); 
       System.out.print("\033[D");              
@@ -75,7 +73,7 @@ public class Line {
   }
 
   @Override
-  public String toString(){
+  public String toString() {
     StringBuilder str = new StringBuilder();
     for (char s : line) 
       str = str.append(s);
