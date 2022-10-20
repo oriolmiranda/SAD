e6c03dc9f4750f9993cf84d0889883e137c6fff8
oriolmiranda <oriolmiranda15@gmail.com>
2022-10-20 18:31:20 +0200

    errors

--- P1_Basic/EditableBufferedReader.java
+++ P1_Basic/EditableBufferedReader.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import P1_MVC.Line;
-
-
 public class EditableBufferedReader extends BufferedReader {
     
     //Constants
--- P1_MVC/EditableBufferedReader.java
+++ P1_MVC/EditableBufferedReader.java
@@ -1,3 +1,4 @@
+package P1_MVC;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
