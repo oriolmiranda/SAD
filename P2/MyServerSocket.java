5c060aec8d833a19c15b661201b376f2abf65e1b
oriolmiranda <oriolmiranda15@gmail.com>
2022-11-11 16:23:32 +0100

    submit

--- P2/MyServerSocket.java
+++ P2/MyServerSocket.java
@@ -3,8 +3,31 @@ import java.net.ServerSocket;
 
 public class MyServerSocket extends ServerSocket {
 
-    public MyServerSocket() throws IOException {
-        super();
+    MySocket mySocket;
+
+    
+    public MyServerSocket(int port) throws IOException {
+
+        super(port);
     }
 
+    @Override
+    public MySocket accept() {
+
+        try {
+            mySocket = new MySocket(super.accept());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mySocket;
+    }
+
+    public void close() {
+        
+        try {
+            super.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
